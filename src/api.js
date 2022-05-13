1b1207ddae538cb3a3372ffc590d7194307d7e27
= <=>
2022-05-13 21:02:36 +0600

    add mongo db upload and get routes

--- src/api.js
+++ src/api.js
@@ -1,37 +1,66 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require('cors')
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config()
 
 const app = express();
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const router = express.Router();
 
+const requestSchema = new mongoose.Schema({
+  page: String,
+  url: String,
+  headline: String,
+  name: String,
+  phone: String,
+  email: String,
+  date: { type: Date },
+});
+
+const usr = process.env.USER
+const pw = process.env.PASS
+
+async function main(obj) {
+  await mongoose.connect(
+    `mongodb+srv://${usr}:${pw}@cluster0.tvtvx.mongodb.net/orders?retryWrites=true&w=majority`
+  );
+
+  const Order = mongoose.model("Order", requestSchema);
+  const ord = new Order(obj);
+  await ord.save();
+}
+
 router.get("/", (req, res) => {
+  console.log(req.ips);
   res.json({
-    hello: "hi!"
+    hello: "hi!",
   });
 });
 
-router.get('/test',(req,res) => {
-    res.json({
-        hello: "test!"
-      });
+router.post("/newrequest", async (req, res) => {
+  let d = new Date();
+  d.setTime(d.getTime() + 6 * 60 * 60 * 1000);
+  let obj = req.body;
+  obj.date = d;
 
-})
+  let ress = await main(obj);
+  console.log(ress);
+  res.json({ status: "ok" });
+});
 
-router.post('/newrequest', (req,res) =>{
-    
-    console.log(req.body);
-    res.json({status: 'ok'})
-})
+router.get("/getrequests", async (req, res) => {
+  await mongoose.connect(
+    "mongodb+srv://vasily:1981Febr@cluster0.tvtvx.mongodb.net/orders?retryWrites=true&w=majority"
+  );
+  
+  const Order = mongoose.model("Order", requestSchema);
 
-router.post('/testpost',(req,res) => {
-    res.json({
-        hello: "hit the POST!"
-      });
-})
+  let orders = await Order.find();
+  console.log(orders);
+});
 
 app.use(`/.netlify/functions/api`, router);
 
